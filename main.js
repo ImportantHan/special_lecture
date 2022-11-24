70510a8c67e00e05fee71071195d8224177e83cd
moo <dodora22@naver.com>
2022-11-24 17:51:44 +0900

    그림판 기능 구현중

--- main.js
+++ main.js
@@ -12,3 +12,31 @@
       } 
     }); 
 })( jQuery );
+
+// =========================================================================
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.rect(50, 50, 100, 100);
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(150, 150, 100, 100);
+ctx.fillStyle = "red"
+ctx.fill();
+
+// shortcut function이 아닌 function
+ctx.moveTo(250, 250);
+ctx.lineTo(350, 250);
+ctx.lineTo(350, 350);
+ctx.lineTo(250, 350);
+ctx.lineTo(250, 250);
+ctx.stroke();
+
+ctx.arc(50, 50, 50, 0, 2*Math.PI);
+ctx.fill();
